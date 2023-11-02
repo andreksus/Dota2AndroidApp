4b873107a1e4cb1201d2db1d2a361e1dd86d5794
Андрей Барышев <baryshev_2002@inbox.ru>
2023-11-02 23:58:08 +0600

    Header (1 screen)

--- app/src/main/java/com/example/dota2androidapp/MainActivity.kt
+++ app/src/main/java/com/example/dota2androidapp/MainActivity.kt
@@ -3,14 +3,40 @@ package com.example.dota2androidapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.dota2androidapp.ui.theme.Dota2AndroidAppTheme
+import kotlin.math.max
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Header()
                 }
             }
         }
@@ -32,7 +58,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Привет $name!",
         modifier = modifier
     )
 }
@@ -41,6 +67,59 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Dota2AndroidAppTheme {
-        Greeting("Android")
+        Greeting(stringResource(id = R.string.app_name))
     }
+}
+//Preview
+@Preview
+@Composable
+fun DotaAppPreview() {
+    Header()
+}
+
+@Preview
+@Composable
+fun CurrentlyPreview(){
+    MainPicture()
+}
+
+//Components
+@Composable
+fun MainPicture(){
+    Image(
+        painterResource(id = R.drawable.dota_main_picture),
+        contentDescription = null,
+        contentScale = ContentScale.FillWidth,
+        modifier = Modifier
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun Icon(){
+    Image(
+        painterResource(id = R.drawable.icon_box),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxHeight()
+            .size(140.dp)
+            .padding(top = 240.dp)
+    )
+}
+
+@Composable
+fun Header(){
+    Box(
+        modifier = Modifier
+            .background(color = colorResource(id = R.color.background))
+            .height(450.dp)
+    ){
+        MainPicture()
+        Icon()
+    }
+}
+
+@Composable
+fun Body(){
+
 }
