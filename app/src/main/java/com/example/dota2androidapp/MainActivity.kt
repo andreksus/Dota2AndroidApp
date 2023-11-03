59a412f6ee8175f96388ae55fe569b7abb3bf4ce
Андрей Барышев <baryshev_2002@inbox.ru>
2023-11-03 12:44:09 +0600

    1 Screen

--- app/src/main/java/com/example/dota2androidapp/MainActivity.kt
+++ app/src/main/java/com/example/dota2androidapp/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.dota2androidapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -18,13 +22,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -40,6 +49,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dota2androidapp.ui.theme.Dota2AndroidAppTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import kotlin.math.max
 
 class MainActivity : ComponentActivity() {
@@ -52,7 +62,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Header()
+                    Body()
                 }
             }
         }
@@ -84,8 +94,10 @@ fun DotaAppPreview() {
 @Preview
 @Composable
 fun CurrentlyPreview(){
-    Description()
+    InstallButton()
 }
+
+
 
 //Components
 @Composable
@@ -116,7 +128,7 @@ fun Header(){
     Box(
         modifier = Modifier
             .background(color = colorResource(id = R.color.background))
-            .height(450.dp)
+            .height(420.dp)
     ){
         MainPicture()
         Icon()
@@ -131,6 +143,8 @@ fun Body(){
     ) {
         Header()
         Description()
+        ListPictures()
+        InstallButton()
     }
 }
 
@@ -147,4 +161,42 @@ fun Description(){
 }
 
 @Composable
+fun CardPicture(imageId: Int){
+    Image(
+        painter = painterResource(id = imageId),
+        contentDescription = null,
+        modifier = Modifier
+            .size(height = 135.dp, width = 240.dp)
+            .clip(shape = RoundedCornerShape(30.dp))
+    )
+}
+
+@Composable
+fun ListPictures(imagesId: List<Int> = listOf(R.drawable.image_18, R.drawable.image_18)){
+    LazyRow(
+        contentPadding = PaddingValues(30.dp),
+        horizontalArrangement = Arrangement.spacedBy(20.dp)
+    ){
+        items(imagesId){imageId ->
+            CardPicture(imageId = imageId)
+        }
+    }
+}
+
+@Composable
+fun InstallButton(){
+        Button(
+            onClick = { /*TODO*/ },
+            contentPadding = PaddingValues(20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.button))
+        ) {
+        Text("Install", color = colorResource(id = R.color.text_button), fontSize = 20.sp)
+    }
+}
+
+
 
