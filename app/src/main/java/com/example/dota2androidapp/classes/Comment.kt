5d2037d93b73632bf2ad1df99b6350d38781b031
Андрей Барышев <baryshev_2002@inbox.ru>
2023-11-09 15:28:47 +0600

    Added classes: user, date, comment for comments elements

---
+++ app/src/main/java/com/example/dota2androidapp/classes/Comment.kt
@@ -0,0 +1,7 @@
+package com.example.dota2androidapp.classes
+
+class Comment(user: User, date: Date, text: String) {
+    val user = user
+    val date = date
+    val text = text
+}
---
+++ app/src/main/java/com/example/dota2androidapp/classes/Date.kt
@@ -0,0 +1,7 @@
+package com.example.dota2androidapp.classes
+
+class Date(month: String, day: String, year: String) {
+   val month = month
+   val day = day
+   val year = year
+}
